c7239d2602c6e562af9f6054b373800e562b5157
rohitbadgurjar <rohit004052@gmail.com>
2026-08-05 11:29:13 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -24,7 +24,7 @@ console.log("TC-03 : script.js exists:FAIL");
 passed=false;
 }
 if(fs.existsSync("data/students.json")) {
-    console.log("TC-4 : students.json exists:PASS");
+    console.log("TC-04 : students.json exists:PASS");
  }
 else{
 console.log("TC-04 : students.json exists:FAIL");
@@ -34,3 +34,41 @@ const students = JSON.parse(
     fs.readFileSync("data/students.json")
 );
 const student=student[0];
+if(student.email.includes("@")) {
+    console.log("TC-06 : Email Validation:PASS");
+ }
+else{
+console.log("TC-06 : Email Validation:FAIL");
+passed=false;
+}
+if(student.mobile.includes("@")) {
+    console.log("TC-07 : Mobile Validation:PASS");
+ }
+else{
+console.log("TC-07 : Mobile Validation:FAIL");
+passed=false;
+}
+if(student.branch!==""){
+    console.log("TC-08 : Branch Validation:PASS");
+ }
+else{
+console.log("TC-08 : Branch Validation:FAIL");
+passed=false;
+}
+if(student.password.length>=6){
+    console.log("TC-09 : Password Validation:PASS");
+ }
+else{
+console.log("TC-09 : Password Validation:FAIL");
+passed=false;
+}
+if(passed){
+    console.log("TC-10: Registration Successful:PASS");
+    console.log("\nBuild SUCCESS");
+   process.exit(0);
+ }
+else{
+console.log("TC-10 : Registration Successful:FAIL");
+console.log("\nBuild FAILED");
+process.exit(1);
+}
